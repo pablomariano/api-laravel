cd242104950edc44550f5bfe7a520316c90c3fee
Pablo <pablomariano@gmail.com>
2024-03-02 16:09:58 -0300

    add store customer request

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Models\Customer;
-use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerResource;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Filters\V1\CustomersFilter;
 use Illuminate\Http\Request;
+use App\Http\Requests\V1\StoreCustomerRequest;
 
 class CustomerController extends Controller
 {
@@ -33,20 +33,13 @@ class CustomerController extends Controller
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(StoreCustomerRequest $request)
     {
-        //
+        return new CustomerResource(Customer::create($request->all()));
     }
 
     /**
@@ -61,14 +54,6 @@ class CustomerController extends Controller
         }
 
         return new CustomerResource($customer);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Customer $customer)
-    {
-        //
     }
 
     /**
